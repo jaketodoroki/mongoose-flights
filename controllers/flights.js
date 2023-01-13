e8cf07ad48b893b69bca8d33fc9e07b39dee7e15
JakeTodoroki <taco7897@gmail.com>
2023-01-12 20:47:15 -0700

    add index controller and functionality

--- controllers/flights.js
+++ controllers/flights.js
@@ -6,6 +6,33 @@ function newFlight(req, res){
   })
 }
 
+function create(req, res){
+  Flight.create(req.body)
+  .then(flight => {
+    res.redirect('/flights/new')
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/flights/new')
+  })
+}
+
+function index(req, res) {
+  Flight.find({})
+  .then(flights => {
+    res.render('flights/index', {
+      flights: flights,
+      title: 'All Flights'
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
+
 export {
-  newFlight as new
+  newFlight as new,
+  create,
+  index
 }
